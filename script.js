ad25712d7241abf69118d23e83da43da3b67f5b4
Shatha3004 <Shatha2004@gmail.com>
2025-03-02 23:31:51 +0530

    first add

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const toggleBtn = document.getElementById("toggleBtn");
+    const closeBtn = document.getElementById("closeBtn");
+
+    toggleBtn.addEventListener("click", function () {
+        sidebar.classList.add("active");
+    });
+
+    closeBtn.addEventListener("click", function () {
+        sidebar.classList.remove("active");
+    });
+});
